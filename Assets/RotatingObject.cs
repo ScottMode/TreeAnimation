66f526c272f98f1e40c1cfffe7d35df68acb2b06
Nelson Scott <nelson@scottmode.com>
2018-04-19 14:33:57 -0400

    Final product

--- Assets/RotatingObject.cs
+++ Assets/RotatingObject.cs
@@ -9,20 +9,26 @@ public class RotatingObject : MonoBehaviour {
 
 	//Assigned in inspector
 	public Spray leaves;
+	public Spray rocks;
 	public ReaktorLink reaktor;
 
 	//Logic
 	bool animating = false;
 	float animationTimer = 0f;
 
+	//Building and tree game objects
 	public GameObject building;
 	public GameObject tree;
 
 	void Awake() {
+		//Init the audio plugin
 		reaktor.Initialize(this);
 
+		//Set the tree to inactive and building to active
 		tree.SetActive (false);
 		building.SetActive (true);
+
+		rocks.scale = 0f;
 
 		//Logic
 		animating = false;
@@ -30,66 +36,80 @@ public class RotatingObject : MonoBehaviour {
 	
 	void Update () {
 
-		//Rotate the object
+		//Rotate the entire object slowly
 		transform.Rotate(transform.up, Time.deltaTime * 5);
 
-		//Update based on volume
+		//Update based on db level. It's a float 0-1f
 		float ro = reaktor.Output;
-		//Debug.Log ("Output: " + ro);
 
 		//Animation timing logic
-		if (ro > 0.2f) {
+		if (ro > 0.1f) {
 			//Begin animating
 			if (animationTimer <= 0f) {
 				animating = true;
 			}
 
-			//Scale
+			//Set scale of leaves according to animation
 			if (animating && animationTimer == 0f) {
-				leaves.scale = ro * 3f;
+
+				if (ro < 0.5f) {
+					leaves.scale = ro * 1f;
+				} else {
+					leaves.scale = ro * 3f;
+				}
+
 			}
 
-			if (ro >= 0.85f) {
-				animationTimer = 4f;
+			//Animation reaches peak db
+			if (ro >= 0.95f) {
+				animationTimer = 3f;
+				//Set fixed leaves position
 				leaves.gameObject.GetComponent<TransformGear> ().position.min = -1;
+				rocks.scale = 0f;
+
 				//change to tree
 				tree.SetActive(true);
 				building.SetActive (false);
 			}
-		} 
-		else {
-			//Check to end animation
-			if (animationTimer > 0f) { 
-				animationTimer -= Time.deltaTime;
+		}
 
-				Debug.Log (animationTimer);
+		//Check to end animation
+		if (animationTimer > 0f) { 
+			animationTimer -= Time.deltaTime;
 
-				if (leaves.scale > 0) {
-					leaves.scale -= Time.deltaTime * 0.6f;
+			//Slowly decrease scale of leaves
+			if (leaves.scale > 0) {
+				leaves.scale -= Time.deltaTime * 0.5f;
 
-					if (leaves.scale < 0f) { 
-						leaves.scale = 0f;
-					}
-				}
-
-				if (animationTimer <= 0f) {
-					animating = false;
+				if (leaves.scale < 0f) { 
 					leaves.scale = 0f;
-					animationTimer = 0f;
-
-					tree.SetActive(false);
-					building.SetActive (true);
-
-					//Change to building
-					leaves.gameObject.GetComponent<TransformGear> ().position.min = -3;
 				}
+			}
+
+			rocks.scale += Time.deltaTime * 0.4f;
+
+			if (animationTimer <= 0f) {
+				//Set leaves scale to zero and reset animation logic
+				animating = false;
+				leaves.scale = 0f;
+				animationTimer = 0f;
+
+				//Set building to active
+				tree.SetActive(false);
+				building.SetActive (true);
+
+				//Set leaves position to bottom
+				leaves.gameObject.GetComponent<TransformGear> ().position.min = -3;
 			}
 		}
 
+		//Scale rocks down if animation is done
+		if (rocks.scale > 0f && animationTimer == 0f) {
+			rocks.scale -= Time.deltaTime * 1f;
 
-
-		//Radius
-
-		//Position
+			if (rocks.scale < 0f) {
+				rocks.scale = 0f;
+			}
+		}
 	}
 }
